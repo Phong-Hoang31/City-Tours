e440d2c323463c7a1699b7ac4e554b37b37b2097
John Butcher <1butcherjohn@gmail.com>
2022-08-08 12:56:18 -0400

    Adding landmarks, images table to database schema.sql

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, landmarks, images;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,20 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE landmarks (
+    landmark_id SERIAL PRIMARY KEY,
+    landmark_name varchar(50) NOT NULL,
+    venue varchar(50) NOT NULL,
+    description varchar(500) NOT NULL,
+    up_ratings integer,
+    down_ratings integer
+    );
+
+CREATE TABLE images (
+    landmark_id integer FOREIGN KEY,
+    url varchar(200) PRIMARY KEY
 );
 
 COMMIT TRANSACTION;
