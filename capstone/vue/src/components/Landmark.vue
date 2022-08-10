658e94d13e0b1e3b3b6c1b9831ac3dfd8b2a687a
John Butcher <1butcherjohn@gmail.com>
2022-08-10 12:06:14 -0400

    Merge resolved

--- capstone/vue/src/components/Landmark.vue
+++ capstone/vue/src/components/Landmark.vue
@@ -1,54 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="landmark" v-bind:key="landmark.id">
-    <h4>{{ landmark.landmarkName }}</h4>
-    <div class="photos">
-      <img />
-    </div>
-    <h3>{{ landmark.category }}</h3>
-    <p>{{ landmark.description }}</p>
-  </div>
-</template>
-
-<script>
-</script>
-
-<style>
-div.main div.review {
-  border: 1px black solid;
-  border-radius: 6px;
-  padding: 1rem;
-  margin: 10px;
-}
-
-div.main div.review.favorited {
-  background-color: lightyellow;
-}
-
-div.main div.review div.rating {
-  height: 2rem;
-  display: inline-block;
-  vertical-align: top;
-  margin: 0 0.5rem;
-}
-
-div.main div.review div.rating img {
-  height: 100%;
-}
-
-div.main div.review p {
-  margin: 20px;
-}
-
-div.main div.review h3 {
-  display: inline-block;
-}
-
-div.main div.review h4 {
-  font-size: 1rem;
-}
-</style>
-=======
   <div class="wrapper">
     <v-card class="mx-auto" width="400">
       <v-img
@@ -150,4 +100,3 @@ button {
   max-width: 500px;
 } */
 </style>
->>>>>>> a441677465ff3de7d7dbd7783162358a5bb15601
