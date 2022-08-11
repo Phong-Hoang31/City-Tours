09e0f51866431c95bde1383fee585cee59ac232f
John Butcher <1butcherjohn@gmail.com>
2022-08-11 10:43:37 -0400

    Style changes for filter

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -8,21 +8,16 @@
 
 <script>
 import LandmarkList from "@/components/LandmarkList.vue";
-import SidebarDisplay from "@/components/Sidebar.vue"
+import SidebarDisplay from "@/components/Sidebar.vue";
 
 export default {
   name: "Home",
   components: {
     LandmarkList,
-    SidebarDisplay
+    SidebarDisplay,
   },
 };
 </script>
-<<<<<<< HEAD
-<style scoped>
-
-</style>
-=======
 
 <style scoped>
 h3 {
@@ -30,4 +25,3 @@ h3 {
   letter-spacing: 0.75rem;
 }
 </style>
->>>>>>> d891ccb08ba5636e6f332dddfe6f2e3986a224fc
