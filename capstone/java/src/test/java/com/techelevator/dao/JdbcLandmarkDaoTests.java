8e0e59be0879a2504e24018c6b27a21a7f59d5c6
Phong <hoagphog31@gmail.com>
2022-08-17 11:15:02 -0400

    wrote DAO test

--- capstone/java/src/test/java/com/techelevator/dao/JdbcLandmarkDaoTests.java
+++ capstone/java/src/test/java/com/techelevator/dao/JdbcLandmarkDaoTests.java
@@ -1,5 +1,7 @@
 package com.techelevator.dao;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.techelevator.model.Landmark;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +20,24 @@ public class JdbcLandmarkDaoTests extends BaseDaoTests {
     @Test
     public void getLandmarks_returns_all_landmarks_is_not_empty() {
         Assert.assertNotNull(sut.getLandmarks());
+    }
+    @Test
+    public void getLandmarks_returns_correct_landmark_for_id(){
+        Landmark landmark= sut.getLandmarksById(1);
+    }
+
+    private void assertLandmarkMatch(Landmark expected, Landmark actual){
+        Assert.assertEquals(expected.getLandmarkID(),actual.getLandmarkID());
+        Assert.assertEquals(expected.getLandmarkName(),actual.getLandmarkName());
+        Assert.assertEquals(expected.getAddress(),actual.getAddress());
+        Assert.assertEquals(expected.getCategory(),actual.getCategory());
+        Assert.assertEquals(expected.getDescription(),actual.getDescription());
+        Assert.assertEquals(expected.getImageUrlList(),actual.getImageUrlList());
+        Assert.assertEquals(expected.getLandmarkOrder(),actual.getLandmarkOrder());
+        Assert.assertEquals(expected.getPrice(),actual.getPrice());
+        Assert.assertEquals(expected.getUpRatings(),actual.getUpRatings());
+        Assert.assertEquals(expected.getDownRatings(),actual.getDownRatings());
+        Assert.assertEquals(expected.getScheduleList(),actual.getScheduleList());
     }
 
 }
